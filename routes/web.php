98ded4e47c32cd26caa3c574e4512337e9a77e15
rismayo <rismamanura855@gmail.com>
2024-10-07 09:55:11 +0700

    welcome.php umkm

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/shop', function () {
+    return view('shop');
+});
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+Route::get('/404', function () {
+    return view('404');
+});
+Route::get('/cart', function () {
+    return view('cart');
+});
+Route::get('/chackout', function () {
+    return view('chackout');
+});
+Route::get('/shop-detail', function () {
+    return view('shop-detail');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
