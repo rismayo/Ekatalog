434b33af30e777a9f610719b95281482af2142ab
shenialusi <shenia.lusi2004@gmail.com>
2024-10-20 21:28:16 +0700

    tes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use App\Http\Controllers\AdminController;
@@ -18,13 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/superadmin/crudsuperadmin', function () {
+Route::get('/crudsuperadmin', function () {
     return view('superadmin.crudsuperadmin');
 });
-Route::get('/superadmin/lihatsuperadmin', function () {
+Route::get('/lihatsuperadmin', function () {
     return view('superadmin.lihatsuperadmin');
 });
-Route::get('/superadmin/dashboard', function () {
+Route::get('/dashboard', function () {
     return view('superadmin.dashboard');
 });
 Route::get('/login', [SesiController::class, 'dashboard']);
