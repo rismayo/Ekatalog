381c5d4df33f889e0e4cd759688a80fd5b84a749
RahmadinaMFA <rahmadinaandani24@gmail.com>
2024-10-21 07:41:09 +0700

    perbaiki route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/dashboard', function () {
+    return view('superadmin.dashboard');
 });
 Route::get('/superadmin/crudsuperadmin', function () {
     return view('superadmin.crudsuperadmin');
@@ -24,9 +26,17 @@ Route::get('/superadmin/crudsuperadmin', function () {
 Route::get('/superadmin/lihatsuperadmin', function () {
     return view('superadmin.lihatsuperadmin');
 });
-Route::get('/superadmin/dashboard', function () {
-    return view('superadmin.dashboard');
+Route::get('/umkm/crudumkm', function () {
+    return view('umkm.crudumkm');
 });
-Route::get('/login', [SesiController::class, 'dashboard']);
-Route::post('/login', [SesiController::class, 'login']);
-Route::get('/superadmin', [AdminController::class, 'dashboard']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [SesiController::class, 'dashboard'])->name('login');
+    Route::post('/login', [SesiController::class, 'login']);
+});
+Route::get('/home', function () {
+    return redirect('/superadmin/dashboard');
+});
+Route::middleware(['auth'])->group(function(){
+    Route::get('/superadmin', [AdminController::class, 'dashboard']);
+    Route::get('/superadmin/logout', [SesiController::class, 'logout']);
+});
